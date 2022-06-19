631af1a62c61ef8cbae67c2bd758939e970c85a9
den <066den@gmail.com>
2022-06-19 08:31:19 +0300

    add button goback

--- src/pages/PokemonPage/index.tsx
+++ src/pages/PokemonPage/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useEffect } from 'react';
-import { Grid } from '@mui/material';
-import { useParams } from 'react-router-dom';
+import { Button, Grid } from '@mui/material';
+import { useNavigate, useParams } from 'react-router-dom';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 import { useActions } from '../../hooks/useActions';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
@@ -12,22 +13,34 @@ const PakemonPage: FC = () => {
 	const { pokemon } = useTypedSelector(state => state.pokemons);
 	const { getPokemon } = useActions();
 	const { name } = useParams();
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		getPokemon(name);
 	}, []);
 	return (
-		<Grid container>
-			<Grid item xs={12}>
-				<PokemonDetails pokemon={pokemon} />
+		<>
+			<Button
+				variant='outlined'
+				startIcon={<ArrowBackIcon />}
+				color='secondary'
+				onClick={() => navigate('/')}
+				sx={{ position: 'fixed', left: '15px', top: '15px' }}
+			>
+				Go back
+			</Button>
+			<Grid container>
+				<Grid item xs={12}>
+					<PokemonDetails pokemon={pokemon} />
+				</Grid>
+				<Grid item xs={12}>
+					<PokemonStats pokemon={pokemon} />
+				</Grid>
+				<Grid item xs={12}>
+					<PokemonMoves pokemon={pokemon} />
+				</Grid>
 			</Grid>
-			<Grid item xs={12}>
-				<PokemonStats pokemon={pokemon} />
-			</Grid>
-			<Grid item xs={12}>
-				<PokemonMoves pokemon={pokemon} />
-			</Grid>
-		</Grid>
+		</>
 	);
 };
 
